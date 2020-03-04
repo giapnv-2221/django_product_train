9e9ebfa3fa886bf31bcb308b45816a56208fff98
Nguyen Van Giap B <nguyen.van.giapb@sun-asterisk.com>
2020-03-04 10:48:48 +0700

    Fix routing(urls) and create, update, destroy product

--- shopmall/products/urls.py
+++ shopmall/products/urls.py
@@ -5,9 +5,7 @@ app_name = "products"
 urlpatterns = [
     url('^$', views.index, name='index'),
     url('^(?P<product_id>[0-9]+)/$', views.show, name='show'),
-    url(r'^new/$', views.new, name='new'),
-    # url('/', views.create, name='create'),
-    url(r'^edit/(?P<product_id>[0-9]+)/$', views.edit, name='edit'),
-    url(r'^<int:id>/$', views.update, name='update'),
-    url(r'^<int:id>/$', views.destroy, name='destroy'),
+    url('^create/$', views.create, name='create'),
+    url('^(?P<product_id>[0-9]+)/update/$', views.update, name='update'),
+    url('^(?P<product_id>[0-9]+)/destroy/$', views.destroy, name='destroy'),
 ]
--- shopmall/products/views.py
+++ shopmall/products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Products
+from .forms import ProductForm
 
 
-# Create your views here.
 def index(request):
     products = Products.objects.all()
     return render(request, 'products/index.html', {"products": products})
@@ -13,26 +13,36 @@ def show(request, product_id):
     try:
         product = Products.objects.get(id=product_id)
     except:
-        return redirect(index)
+        return redirect('products:index')
 
     return render(request, 'products/show.html', {"product": product})
 
 
 def new(request):
-    return None
+    form = ProductForm()
+    return render(request, 'products/new.html', {"form": form})
 
 
 def create(request):
-    return None
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('products:index')
+    return render(request, 'products/new.html', {"form": form})
 
 
-def edit(request):
-    return None
+def update(request, product_id):
+    product = get_object_or_404(Products, id=product_id)
+    form = ProductForm(request.POST or None, instance=product)
+    if form.is_valid():
+        form.save()
+        return redirect('products:show', product_id=product_id)
+    return render(request, 'products/edit.html', {'form': form})
 
 
-def update(request):
-    return None
-
-
-def destroy(request):
-    return None
+def destroy(request, product_id):
+    product = get_object_or_404(Products, id=product_id)
+    if request.POST:
+        product.delete()
+        return redirect('products:index')
+    return render(request, 'products/destroy.html', {'object': product})
